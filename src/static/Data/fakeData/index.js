7a26621680ea5b4121e1620ed9cd050e438d3d50
aramistamboulian961 <aramistamboulian961@gmail.com>
2023-03-29 22:36:18 +0300

    adding and setting up fakeData for service page

--- src/static/Data/fakeData/index.js
+++ src/static/Data/fakeData/index.js
@@ -1,10 +1,27 @@
 const fakeData = [
     {
         id: '1',
-        name: '',
-        description: '',
+        number: '01',
+        title: 'Brand Identity + Positioning',
+        icon: '/media/icon/arrow.svg',
 
-    }
+    },
+
+    {
+        id: '2',
+        number: '02',
+        title: 'Website Design + Development',
+        icon: '/media/icon/arrow.svg',
+
+    },
+
+    {
+        id: '3',
+        number: '03',
+        title: 'Mobile Design',
+        icon: '/media/icon/arrow.svg',
+
+    },
 ]
 
 export default fakeData;
