a65a65c4275c0125599901f5fa3fbf98ee7d2f9d
aramistamboulian961 <aramistamboulian961@gmail.com>
2023-04-27 12:37:32 +0300

    finished about us page

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -14,63 +14,36 @@ const Nav = () => {
   };
 
   return (
-    <nav>
+    <nav className=''>
       <div className="logo">
-        <img
-          src="/media/logo/ThinkFrimLogo.svg"
-          className="logo-img"
-          alt=""
-        />
+        <a href="/">
+          <img
+            src="/media/logo/ThinkFrimLogo.svg"
+            className="logo-img"
+            alt=""
+          />
+        </a>
       </div>
       <div className={isOpen ? "hamburger toggle" : "hamburger"} onClick={toggleNavbar}>
         <div className="line1"></div>
         <div className="line2"></div>
         <div className="line3"></div>
       </div>
-      <ul className={isOpen ? "nav-links open": "nav-links"}>
-        <li><a href="#" onClick={closeNavbar}>About</a></li>
-        <li><a href="#packages" onClick={closeNavbar}>Services</a></li>
-        <li><a href="#footer" onClick={closeNavbar}>Contact Us</a></li>
+      <ul className={isOpen ? "nav-links open" : "nav-links"}>
+        <li>
+          <HashLink to="/about" className='font-semibold' smooth onClick={closeNavbar}>About</HashLink>
+        </li>
+        <li>
+          <HashLink to="/#service" className='font-semibold' smooth onClick={closeNavbar}>Services</HashLink>
+        </li>
+        <li>
+        <HashLink to="/about" className='font-semibold' smooth onClick={closeNavbar}>Contact Us</HashLink>
+        </li>
         <a href="https://instagram.com/dietbyyara?igshid=MTIyMzRjYmRlZg==">
           <img src="/media/icons/Instagram.svg" alt="" />
         </a>
       </ul>
     </nav>
-    // <nav className='px-2 py-5 chineseBlackBg'>
-    //   <div className="container flex flex-wrap items-center justify-between">
-    //     <a href="/">
-    //       <img className="w-2/3 md:w-full py-5 mx-3" src="/media/logo/ThinkFrimLogo.svg" alt="thinkfrim.svg" />
-    //     </a>
-    //     <div className='flex items-center md:hidden'>
-    //       <Hamburger
-    //         toggled={isOpen}
-    //         onToggle={toggleNavbar}
-    //         duration={0.8}
-    //         direction="right"
-    //         color='#F9BC60'
-    //       />
-    //     </div>
-    //     <div className={` relative w-full md:block md:w-auto ${isOpen ? 'block' : 'hidden'}`} onClick={closeNavbar} id="navbar-default">
-    //       <ul className="absolute w-full  flex flex-col items-center p-5 RajahBg  md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 md:static md:bg-transparent">
-    //         <li className='group Rajah transition-all duration-300 ease-in-out'>
-    //           <HashLink smooth className="block py-2 pl-3 pr-4 chineseBlack font-medium rounded md:hover:bg-transparent md:text-2xl md:border-0 md:p-0 md:text-white md:dark:hover:text-white md:text-[20px] dark:hover:bg-gray-700 bg-left-bottom bg-gradient-to-r from-amber-700 to-amber-700 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out" to="#service">
-    //             Service
-    //           </HashLink>
-    //         </li>
-    //         <li className='group Rajah transition-all duration-300 ease-in-out'>
-    //           <HashLink smooth className="block py-2 pl-3 pr-4 chineseBlack font-medium rounded md:hover:bg-transparent md:text-2xl md:border-0 md:p-0  md:text-white md:dark:hover:text-white md:text-[20px] dark:hover:bg-gray-700 bg-left-bottom bg-gradient-to-r from-amber-700 to-amber-700 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out" to="/About">
-    //             About
-    //           </HashLink>
-    //         </li>
-    //         <li className='group Rajah transition-all duration-300 ease-in-out'>
-    //           <HashLink smooth className="block py-2 pl-3 pr-4 chineseBlack font-medium rounded md:hover:bg-transparent md:text-2xl md:border-0 md:p-0  md:text-white md:dark:hover:text-white md:text-[20px] dark:hover:bg-gray-700 bg-left-bottom bg-gradient-to-r from-amber-700 to-amber-700 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out" to="/Contact">
-    //             Contact us
-    //           </HashLink>
-    //         </li>
-    //       </ul>
-    //     </div>
-    //   </div>
-    // </nav>
   )
 }
 
--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -6,26 +6,26 @@ const About = () => {
     return (
         <div className='chineseBlackBg min-h-screen'>
             <Nav />
-            <div className='lg:pb-32 lg:pt-20 pb-14'>
-                <div className="flex flex-col items-center lg:flex-row lg:justify-start lg:gap-[5rem] xl:gap-[25rem]">
+            <div className='xl:mb-[20rem] xl:mt-[5rem] pt-[10rem]'>
+                <div className="flex flex-col items-center lg:items-start lg:flex-row lg:justify-start lg:gap-[5rem] xl:gap-[20rem]">
                     <div class="relative pb-5">
-                        <img src="/media/illustrator/dotts.svg" class="object-cover w-full" alt="" />
-                        <div class="absolute inset-0 z-0">
-                            <h1 class="text-white text-[5rem] xl:text-[10rem] font-bold uppercase z-10">About</h1>
+                        <img src="/media/illustrator/dotts.svg" class="hidden lg:block object-cover xl:max-w-full" alt="" />
+                        <div class="lg:absolute lg:inset-0">
+                            <h1 class="text-white text-[5rem] lg:text-[7rem] xl:text-[10rem] font-bold uppercase">About</h1>
                         </div>
                     </div>
 
-                    <div className="flex flex-col justify-center text-center lg:text-left">
-                        <p className="text-white text-[16.5px] lg:text-[1.5rem] xl:text-[2.5rem] mt-4 xl:leading-[3rem]">
-                            ThinkFrim Agency is a premium design agency.<br />
-                            We utilize technology and an unmatched , <br /> 
+                    <div className="mb-[22rem] flex flex-col justify-center text-center lg:text-left xl:text-left md:mb-[20rem] sm:mb-[22rem]">
+                        <p className="text-white text-[15px] sm:text-[20px] md:text-[30px] w-full lg:text-[1.5rem] xl:text-[2rem] break-word  mt-4 xl:leading-[3rem]">
+                            ThinkFrim Agency is a premium design agency. <br />
+                            We utilize technology and an unmatched , <br />
                             work ethic to elevate your brand through <br />
                             the web channel.
                         </p>
 
-                        <p class="text-white text-[15.5px] lg:text-[1rem] xl:text-[1.5rem] mt-3 font-light xl:leading-[2rem]">
+                        <p class="text-white text-[15.5px] sm:text-[19px] md:text-[25px] lg:text-[20px] xl:text-[1.5rem] mt-3 font-light xl:leading-[2rem]">
                             We're based out of Beirut, Lebanon with a<br />
-                            consultative & digital production presence
+                            consultative & digital production presence<br />
                             across the continental Lebanon.
                         </p>
 
